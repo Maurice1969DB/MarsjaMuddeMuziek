0e0dbfca16ff076c1d87068c8b3c2302a56395f9
Maurice1969DB <Maurice1969DB@users.noreply.github.com>
2023-06-26 19:40:25 +0000

    deploy: 9d8acca84c431af25969485b63fc6a793eb3f841

--- js/marsja.js
+++ js/marsja.js
@@ -1,5 +1,9 @@
+
 const sidebar = document.querySelector('.sidebar');
 const content = document.querySelector('.content');
+
+sidebar.classList.add('sidebar_small');
+    content.classList.add('sidebar_small');
 
 document.querySelector('.toggle').onclick = function () {
     sidebar.classList.toggle('sidebar_small');
